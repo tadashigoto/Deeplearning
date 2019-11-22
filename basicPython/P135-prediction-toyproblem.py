c9ab3eefcf548ebf1aa1c7e00fe2d29d13baadbc
Tadashi Goto <tadashi.goto2564@gmail.com>
2019-11-22 22:48:42 +0900

    モデル評価実験バグ修正

--- basicPython/P135-prediction-toyproblem.py
+++ basicPython/P135-prediction-toyproblem.py
@@ -2,27 +2,30 @@ import numpy as np
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 from sklearn import datasets
+from sklearn.utils import shuffle
+np.random.seed(0)
+tf.set_random_seed(1234)
 N = 300
 X,y = datasets.make_moons(N,noise=0.3)
 from sklearn.model_selection import train_test_split
 Y = y.reshape(N, 1)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=0.8)
-num_hidden = 2
+num_hidden = 4  
 x = tf.placeholder(tf.float32, shape=[None, 2])
-t = tf.placeholder(tf.float32, shape=[none, 1])
+t = tf.placeholder(tf.float32, shape=[None, 1])
 # 入力値 - 隠れ層
 W = tf.Variable(tf.truncated_normal([2,num_hidden]))
 b = tf.Variable(tf.zeros([num_hidden]))
 h = tf.nn.sigmoid(tf.matmul(x,W)+b)
 # 隠れ層 - 出力層
-V = tf.valiable(tf.truncated_normal([num_hidden]))
+V = tf.Variable(tf.truncated_normal([num_hidden,1]))
 c = tf.Variable(tf.zeros([1]))
-y = tf.nn.sigmoid(if.matnul(h,V)+c)
+y = tf.nn.sigmoid(tf.matmul(h,V)+c)
 
 cross_entropy = - tf.reduce_sum(t * tf.log(y) + (1-t) * tf.log(1-y))
 train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
-correct_prediction = tf.equal(tf.to_float(rf.greater(y,0.5)),t)
-accurace = tf.reduce_men(tf.cast(correct_prediction,tf.float32))
+correct_prediction = tf.equal(tf.to_float(tf.greater(y,0.5)),t)
+accurace = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
 
 batch_size = 20
 n_batches = N // batch_size
